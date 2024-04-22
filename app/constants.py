e6322e09f39640c6911c20807bc48f5fc83c816a
zaritskiyaa <zaritskiyaa@mail.ru>
2024-04-22 15:14:59 +0300

    second review

--- app/constants.py
+++ app/constants.py
@@ -3,6 +3,8 @@ MAX_LEN_NAME = 100
 MIN_LEN = 1
 GT_VALUE = 0
 FORMAT = "%Y/%m/%d %H:%M:%S"
+ROW_COUNT = 100
+COLUMN_COUNT = 11
 SPREADSHEET_WORKPIECE = {
     'properties': {
         'title': 'Отчёт от {now_date_time}',
@@ -14,7 +16,7 @@ SPREADSHEET_WORKPIECE = {
                 'sheetType': 'GRID',
                 'sheetId': 0,
                 'title': 'Лист1',
-                'gridProperties': {'rowCount': 100, 'columnCount': 11},
+                'gridProperties': {'rowCount': ROW_COUNT, 'columnCount': COLUMN_COUNT},
             }
         }
     ],
--- app/utils.py
+++ app/utils.py
@@ -2,5 +2,5 @@ import copy
 from collections.abc import Iterable
 
 
-def get_spreadsheet_json(data) -> Iterable:
+def get_spreadsheet_json(data: Iterable) -> Iterable:
     return copy.deepcopy(data)
